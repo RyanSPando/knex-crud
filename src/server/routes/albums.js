fd194aac0bd2b7eedda7f32283811ec12a49f834
Ryan "archaen" Pando <vretha@gmail.com>
2016-09-06 15:08:31 -0600

    Add page route added.  Migration schema for postgres added.

--- src/server/routes/albums.js
+++ src/server/routes/albums.js
@@ -4,4 +4,12 @@ router.get('/', (req, res, next) => {
   res.render('albums/index', {title: 'Albums'});
 });
 
+router.get('/new', (req, res, next) => {
+  res.render('albums/new', {title: 'New Album'});
+});
+
+router.post('/albums', (req, res, next) => {
+  res.redirect('/albums');
+});
+
 module.exports = router;
